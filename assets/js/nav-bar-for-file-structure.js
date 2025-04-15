8f55a0c6c6353f08e381bab4ff4ad107db00160e
Jiyeon-daisy-kim <Jiyeon-daisy-kim@gmail.com>
2025-04-16 04:25:33 +0900

    네비게이션 바 프리텐다드 체로 변경

--- assets/js/nav-bar-for-file-structure.js
+++ assets/js/nav-bar-for-file-structure.js
@@ -1,3 +1,12 @@
+(function(d) {
+  var config = {
+  kitId: 'qme8xbq',
+  scriptTimeout: 3000,
+  async: true
+  },
+  h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+})(document);
+
 class NavBar extends HTMLElement {
   constructor() {
     super();
@@ -13,7 +22,7 @@ class NavBar extends HTMLElement {
           z-index: 1000;
           position: fixed;
           width: 80%;
-          font-family: 'Montserrat', sans-serif;
+          font-family: 'Pretendard', sans-serif;
         }
 
         .site-name {
